e153a93530104b61232e243ee0e3450622b83a15
leandroucha <leandro.ucha@rockstarcoders.com>
2020-06-03 11:42:28 -0300

    legui on vue

--- 08_udemy/11_cli/03_leguizamon/src/App.vue
+++ 08_udemy/11_cli/03_leguizamon/src/App.vue
@@ -1,32 +1,68 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <a href="https://api.whatsapp.com/send?phone=5493413003771&text=Hola%21+Cu%C3%A1ndo+puedo+pasar+a+dejarles+el+auto%3F" class="float" target="_blank">
+      <i class="fa fa-whatsapp my-float"></i>
+      </a>
+    <!-- Header -->
+    <nav id="mainNavbar" class="navbar navbar-expand-lg navbar-dark bg-red sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#main">
+                <img src="/assets/logo.png" height="50px" alt=""> 
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#main">¿Quiénes somos?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#servicios">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/#contacto">Contacto</a>
+                    </li>
+                     <li class="nav-item">
+                        <router-link to="/about" class="nav-link">About</router-link> 
+                    </li> 
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- /Header -->
+    
     <router-view/>
+    <!-- Footer -->
+    <footer id="footer" class="pb-4 pt-4">
+        <div class="container">
+            <div class="row text-center">
+                <!-- <div class="col-12 col-lg">
+                    <a href="#">Preguntas Frecuentes</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Contactanos</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Prensa</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Conferencias</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Terminos y condiciones</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Privacidad</a>
+                </div>
+                <div class="col-12 col-lg">
+                    <a href="#">Estudiantes</a>
+                </div>-->
+            </div> 
+        </div>
+    </footer>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- 08_udemy/11_cli/03_leguizamon/src/views/Home.vue
+++ 08_udemy/11_cli/03_leguizamon/src/views/Home.vue
@@ -1,7 +1,203 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <!-- Main -->
+    <section class="pb-4">
+        <div id="carousel" class="carousel slide carousel-fade"  data-ride="carousel" data-pause="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/imagen1.png" class="d-block w-100" alt="Imagen 1">
+                </div><div class="carousel-item">
+                    <img src="../assets/imagen2.jpeg" class="d-block w-100" alt="Imagen 2">
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/imagen3.jpeg" class="d-block w-100" alt="Imagen 3">
+                </div>
+                <!-- 
+                <div class="carousel-item">
+                    <img src="../assets/imagen4.png" class="d-block w-100" alt="Imagen 4">
+                </div> -->
+                <div class="overlay py-2">
+                    <div class="container h-100">
+                        <div class="row align-items-center h-100">
+                            <div class="col-lg-3 text-center text-lg-left">
+                                <h1>Servicio Mecánico <span class="bold">Leguizamón</span></h1>
+                                <p>Con más de 43 años de experiencia, realizamos cualquier tipo de reparación y mantenimiento. Queremos que su seguridad no se vea comprometida y la vida útil de su vehículo se prolonge lo máximo posible.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /Main -->
+    
+    
+    <section id="servicios" class="py-4">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h2 class="text-uppercase semi-bold">Servicios</h2>
+          </div>
+        </div>
+         <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up">
+              <div class="box-part text-center desaturate">
+                <img  src="../assets/icon_01.png" alt="" class="py-2"><div class="title">
+                  <h4  class="semi-bold">Inyeccion electrónica</h4>
+                </div>
+                <div class="text">
+                  <span>Limpieza de inyectores, puesta a punto, adaptacion de parámetros.</span>
+                </div>                                 
+              </div>
+          </div>	 
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="50">
+            <div class="box-part text-center desaturate">
+            <img  src="../assets/icon_02.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Diagnóstico Computarizado</h4>
+              </div>
+              <div class="text">
+                <span>Análisis y verificación de fallas electrónicas utilizando sistemas OBDI, OBDII.</span>
+              </div>                      
+             </div>
+          </div>	 
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="100">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_03.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Cambio de correas</h4>
+              </div>
+              <div class="text">
+                <span>Cambio de correas de distribucion, poly-v, rodillos y tensores.</span>
+              </div>                     
+             </div>
+          </div>	 
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="150">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_04.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Frenos</h4>
+              </div>
+              <div class="text">
+                <span>Cambio de discos y pastillas, rectificación de discos, reparación y cambio de calipers.</span>
+              </div>                 
+             </div>
+          </div>	 
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="200">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_05.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Embragues</h4>
+              </div>
+              <div class="text">
+                <span>Cambio de placa y disco, ruleman de empuje, convencionales y bimasa.</span>
+              </div>              
+             </div>
+          </div>	 
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="250">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_06.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Mantenimiento</h4>
+              </div>
+              <div class="text">
+                <span>Cambio de aceite, lubricantes, filtros de aire, aceite, combustible y habitáculo.</span>
+              </div>            
+             </div>
+          </div>
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="300">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_07.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Tren delantero y trasero</h4>
+              </div>
+              <div class="text">
+                <span>Cambio y revisión de bujes, rulemanes, amortiguadores, rotulas, extremos.</span>
+              </div>            
+             </div>
+          </div>
+           <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12"  data-aos="fade-up" data-aos-delay="350">
+            <div class="box-part text-center desaturate">
+              <img  src="../assets/icon_08.png" alt="" class="py-2"><div class="title">
+                <h4  class="semi-bold">Baterías</h4>
+              </div>
+              <div class="text">
+                <span>Colocacion de baterias, control de carga de alternador, cambio y revisión.</span>
+              </div>            
+             </div>
+          </div>
+      </div>		
+      </div>
+  
+    </section>
+    <!-- estado de reparación -->
+    <!-- <div class="container py-4" id="reparacion">
+      <div class="row">
+        <div class="col">
+          <h2 class="text-uppercase">Estado de reparación</h2>
+          <p>Para saber el estado de su reparación, ingrese la patente de su vehículo y el código personal que se le envió por mensaje de Whatsapp.</p>
+        </div>
+      </div>
+      <div class="row py-4">
+        <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="patente">Patente</label>
+                  <input type="text" class="form-control" id="patente" aria-describedby="patenteHelp">
+                  <small id="patenteHelp" class="form-text text-muted">Ingrese la patente sin espacios, ni guiones.</small>
+                </div>
+                <div class="form-group">
+                  <label for="idReparacion">Código de reparación</label>
+                  <input type="text" class="form-control" id="idReparacion" aria-describedby="reparacionHelp">
+                  <small id="reparacionHelp" class="form-text text-muted">Ingrese el código que le enviamos.</small>
+                </div>
+                <a href="reparacion.html" type="submit" class="btn btn-red">Ver estado de reparación</a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- /estado de reparación -->
+
+    <!-- contacto -->
+    <div class="contacto pt-4" id="contacto">
+      <div class="container">
+        <div class="row pb-4">
+          <div class="col">
+            <h2 class="text-uppercase semi-bold">Contacto</h2>
+            <p>Utilice cualquiera de los medios detallados a continuación para comunicarse con nosotros.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-clock-o fa-stack-1x"></i>
+            </span>
+            <span>Lunes a Viernes - 8.00 a 17.00</span> <br><br><br>
+            
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-map-marker-alt fa-stack-1x"></i>
+            </span>
+            <a target="_blank" href="https://www.google.com/maps?ll=-32.958537,-60.667909&z=14&t=m&hl=en&gl=AR&mapclient=embed&cid=3208777413178355326">Riobamba 2973, Rosario, Santa Fe</a> <br><br><br>
+            <span class="fa-stack fa-lg">
+              <i class="fa fas fa-phone fa-stack-1x"></i>
+            </span>
+            <a href="tel:+543414324090">+54 341 432-4090</a><br><br><br>
+            
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-mobile-alt fa-stack-1x"></i>
+            </span>
+            <a href="tel:+5493413003771">+54 9 341 300-3771</a><br><br><br>
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-envelope fa-stack-1x"></i>
+            </span>
+            <a href="mailto:contacto@servicioleguizamon.com.ar">contacto@servicioleguizamon.com.ar</a>
+          </div>
+          <div class="col-lg-6 pb-4">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13390.859386314743!2d-60.6679094!3d-32.9585372!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c87de0e73caee7e!2sTaller%20Leguizamon!5e0!3m2!1sen!2sar!4v1589207251445!5m2!1sen!2sar" width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /contacto -->
   </div>
 </template>
 
